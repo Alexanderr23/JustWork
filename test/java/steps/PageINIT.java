1fe64244919fb4aab72c7ce95a478a8380e2b26d
Alexanderr23 <ar27109@gmail.com>
2022-08-29 19:06:06 -0400

    Basic Frame Work

---
+++ test/java/steps/PageINIT.java
@@ -0,0 +1,14 @@
+package steps;
+
+import pages.LoginPage;
+
+public class PageINIT {
+
+
+        public static LoginPage login;
+
+        public static void initPageObjects(){
+            login=new LoginPage();
+
+        }
+}
